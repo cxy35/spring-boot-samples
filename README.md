d3f6a27d31bae1554bb3981b76c9982dff2147fe
zhengjian511 <454407628@qq.com>
2019-10-28 11:46:49 +0800

    spring-boot-helloworld

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 |示例代码|相关文章|
 |:-|:-|
-|spring-boot-helloworld|Hello World 示例|
+|[spring-boot-helloworld](https://github.com/zhengjian511/spring-boot-samples/tree/master/spring-boot-helloworld)|[创建一个 Spring Boot 项目，你会几种方法？](http://springboot.javaboy.org/2019/0412/springboot-init)|
 
 ## 文章索引
---
+++ spring-boot-helloworld/src/main/java/com/zhengjian/sample/springboot/helloworld/SpringBootHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.zhengjian.sample.springboot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloworldApplication.class, args);
+    }
+
+}
