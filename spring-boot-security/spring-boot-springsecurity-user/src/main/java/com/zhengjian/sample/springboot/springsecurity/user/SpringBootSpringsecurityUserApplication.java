e3f5e5abb43dd8cebaf9b5b2f8b66820d49b9461
zhengjian511 <454407628@qq.com>
2019-11-22 20:19:24 +0800

    Spring Boot 整合 Spring Security（用户名密码配置）

---
+++ spring-boot-security/spring-boot-springsecurity-user/src/main/java/com/zhengjian/sample/springboot/springsecurity/user/SpringBootSpringsecurityUserApplication.java
@@ -0,0 +1,13 @@
+package com.zhengjian.sample.springboot.springsecurity.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringsecurityUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringsecurityUserApplication.class, args);
+    }
+
+}
