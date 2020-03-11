f31f7823bb67a730eb656617b18af3fd5baeed48
cyx35 <799737179@qq.com>
2020-03-11 16:00:22 +0800

    Spring Boot 整合 MyBatis 多数据源

--- spring-boot-dao/spring-boot-mybatismulti/src/main/java/com/cxy35/sample/springboot/mybatismulti/config/MyBatisConfigOne.java
+++ spring-boot-dao/spring-boot-mybatismulti/src/main/java/com/cxy35/sample/springboot/mybatismulti/config/MyBatisConfigOne.java
@@ -4,17 +4,19 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
 @MapperScan(basePackages = "com.cxy35.sample.springboot.mybatismulti.mapper1", sqlSessionFactoryRef = "sqlSessionFactory1", sqlSessionTemplateRef = "sqlSessionTemplate1")
 public class MyBatisConfigOne {
     // 此时 Spring 容器中有两个 DataSource 类型的 Bean ，所以这里需要按名称 byName 查找
-    @Resource(name = "dsOne")
+    @Autowired
+    @Qualifier("dsOne")
     DataSource dsOne;
 
     @Bean
