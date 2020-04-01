c5f43aec322f897ea4a743d1190e465f4298d01b
cyx35 <799737179@qq.com>
2020-04-01 16:02:04 +0800

    Spring Boot 整合 Shiro

---
+++ spring-boot-security/spring-boot-shriostarter/src/test/java/com/cxy35/sample/springboot/shriostarter/SpringBootShriostarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy35.sample.springboot.shriostarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShriostarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
